e9440401143f7ee085a9a2a72836c511a55adabe
Anatolii_Larionov <adlmidl@yandex.ru>
2019-05-13 09:53:24 +0400

    kafka_stream-complete-consumer

--- business-services/employees-api/src/main/java/com/epam/employeesapi/configuration/StreamConfig.java
+++
@@ -1,7 +0,0 @@
-package com.epam.employeesapi.configuration;
-
-import org.springframework.cloud.stream.annotation.EnableBinding;
-
-@EnableBinding(EmployeesStream.class)
-public class StreamConfig {
-}
--- business-services/employees-api/src/main/java/com/epam/employeesapi/controllers/EmployeeAPIController.java
+++ business-services/employees-api/src/main/java/com/epam/employeesapi/controllers/EmployeeAPIController.java
@@ -4,8 +4,9 @@ import com.epam.commons.api.WorkspaceAPI;
 import com.epam.commons.entity.Employee;
 import com.epam.employeesapi.services.EmployeeService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 //import com.epam.employeesapi.dto.EmployeeWithWorkspaceDetails;
 
@@ -19,12 +20,6 @@ public class EmployeeAPIController {
     @RequestMapping("/{id}")
     public Employee describeEmployee(@PathVariable("id") String id) {
         return employeeService.findEmployee(workspaceAPIClient.getWorkspaceById(id).getId());
-    }
-
-    @GetMapping("/")
-    @ResponseStatus(HttpStatus.ACCEPTED)
-    public void setEmployee(@RequestParam("message") String message) {
-
     }
 
 
